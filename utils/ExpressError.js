564d58c144887b66934b15d2b5da413ad14f5f7b
Rahul Patle <rahulpatle1308@gmail.com>
2025-05-29 00:42:49 +0530

    init

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,9 +1,18 @@
+// class ExpressError extends Error {
+//     constructor(statusCode, message) {
+//         super();
+//         this.statusCode = statusCode;
+//         this.message = message;
+//     }
+// }
+
+// module.exports = ExpressError;
 class ExpressError extends Error {
-    constructor(statusCode, message) {
-        super();
-        this.statusCode = statusCode;
-        this.message = message;
-    }
+  constructor(message, statusCode) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
 }
 
 module.exports = ExpressError;
